1e68aaa702a9455cf74c062506db916865565f55
Malgorzata Lesniak <M999@interia.eu>
2019-05-27 19:39:45 +0200

    Correct double case

--- indexZOpisem.js
+++ indexZOpisem.js
@@ -1,13 +1,25 @@
 var os = require('os');
+var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
-var Timeinfo = require ('./modules/Timeinfo')
+var Timeinfo = require ('./modules/Timeinfo');
 
+var emitter = new EventEmitter();
+
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.')
+});
+emitter.on('afterCommand', function() {
+    console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.trim();
+        // odpalanie zdarzenia beforeCommand (z parametrem)
+        emitter.emit('beforeCommand', instruction);
+
         switch(instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -16,12 +28,16 @@ process.stdin.on('readable', function() {
             case '/sayhello':
                 process.stdout.write('hello!\n');
                 break;
-            case '/getOSinfo': 
+            case '/getOSinfo':
+                case '/getOSinfo':
                     OSinfo.print();
                     Timeinfo.changeTimeinfo();
                     break;
             default:
                 process.stderr.write('Wrong instruction!\n');
         };
+        // emitowanie zdarzenia afterCommand (bez parametru)
+        emitter.emit('afterCommand');
+
     }
 });
